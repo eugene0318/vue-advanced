b9e0805dad3d6d11e62b0ea92da9d85ca1640c50
eugene0318 <nttott@gmail.com>
2023-04-14 02:50:57 +0900

    store

--- vue-cli3/src/main.js
+++ vue-cli3/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./routes/index";
+import { store } from "./store";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router, store).mount("#app");
 
 //createApp(App).mount("#app");
---
+++ vue-cli3/src/store/index.js
@@ -0,0 +1,17 @@
+// import Vue from "vue";
+// import Vuex from "vuex";
+import { createStore } from "vuex";
+
+// Vue.use(Vuex);
+
+// export const store = new Vuex.Store({
+//   state: {
+//     news: [],
+//   },
+// });
+
+export const store = createStore({
+  state: {
+    news: [],
+  },
+});
